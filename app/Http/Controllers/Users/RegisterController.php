e932e4243b358e98d5382b89379260c297b85b27
Asa Abbad <asa0abbad@gmail.com>
2020-03-03 00:23:33 +0200

    change register reroute after registeration

--- app/Http/Controllers/Users/RegisterController.php
+++ app/Http/Controllers/Users/RegisterController.php
@@ -25,6 +25,8 @@ class RegisterController extends Controller
     {
         $user = $this->create($request->all());
         event(new UserRegistered($user));
+
+        return redirect()->route('login');
     }
 
     protected function create(array $data)
