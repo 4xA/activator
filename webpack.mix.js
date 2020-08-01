26e5aa8038847a9a7c64eb79a4000b2e48522fde
Asa Abbad <asa0abbad@gmail.com>
2020-08-01 12:19:38 +0300

    Init vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,17 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
- mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/packages/jquery/js')
-    .copy('node_modules/uikit/dist/css', 'public/packages/uikit/css')
-    .copy('node_modules/uikit/dist/js', 'public/packages/uikit/js')
-    .copy('node_modules/uikit/dist/fonts', 'public/packages/uikit/fonts')
-    .copy('resources/fonts/Inconsolata.otf', 'public/fonts')
-    .copy('resources/img', 'public/img')
-    .styles(['resources/css/main.css'], 'public/css/main.min.css')
-    .styles(['resources/css/auth.css'], 'public/css/auth.min.css')
-    .styles(['resources/css/boxes.css'], 'public/css/boxes.min.css')
-    .styles([
-       'node_modules/jquery-toggles/css/toggles.css',
-       'node_modules/jquery-toggles/css/themes/toggles-modern.css'
-    ], 'public/packages/jquery-toggles/css/toggles.min.css')
-    .copy('node_modules/jquery-toggles/toggles.min.js', 'public/packages/jquery-toggles/js');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .copy('node_modules/uikit/dist/css', 'public/packages/uikit/css')
+   .copy('node_modules/uikit/dist/js', 'public/packages/uikit/js')
+   .copy('node_modules/uikit/dist/fonts', 'public/packages/uikit/fonts')
+   .copy('resources/fonts/Inconsolata.otf', 'public/fonts')
+   .copy('resources/img', 'public/img')
+   .styles(['resources/css/main.css'], 'public/css/main.min.css')
+   .styles(['resources/css/auth.css'], 'public/css/auth.min.css')
+   .styles(['resources/css/boxes.css'], 'public/css/boxes.min.css');
